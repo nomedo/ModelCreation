72eb21a7053ea05b861102e2b2cfb284fa3910dd
Nichodemus Omedo <nomedo@fintech-group.com>
2020-02-13 08:03:20 +0300

    first commit

---
+++ ModelCreationNew/ModelCreationNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelCreationNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
